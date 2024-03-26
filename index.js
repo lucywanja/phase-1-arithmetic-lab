22d8bed1bef3b421243394552403e2a204a7205e
Lucy Kariuki <kariuki.lucy@strathmore.edu>
2024-03-26 21:14:09 +0300

    Used operators to perform arithmetic and assign values to variables

--- index.js
+++ index.js
@@ -1,2 +1,28 @@
 
 // Write your code here
+
+// Two numbers that will multiply to be 62
+const num1 = 2;
+const num2 = 31;
+const multiply = num1 * num2;
+
+console.log(multiply);
+
+// generates a random integer greater than 0
+const random = Math.floor(Math.random() * 100) + 1;
+
+console.log(random);
+
+
+// equation that calculates a rem equal to 4
+const num3 = 20;
+const num4 = 8;
+const mod = num3 % num4;
+
+console.log(mod);
+
+// a variable that will return highest number in a set which is 20
+const numbers = [10, 5, 20, 15];
+const max = Math.max(...numbers);
+
+console.log(max);
